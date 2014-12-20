b79d35dbb8b650f90d7d056f8a6dad168fb76726
Deste <dester94@gmail.com>
2014-12-20 10:30:22 +0100

    prova2

---
+++ Scacchi/src/scacchi/Pedina.java
@@ -0,0 +1,11 @@
+package scacchi;
+
+import java.util.ArrayList;
+
+public abstract class Pedina {
+	String name;
+	int color; //1-2
+	
+	public abstract ArrayList<Position> possibleMoves(Position position);
+	
+}
---
+++ Scacchi/src/scacchi/Position.java
@@ -0,0 +1,11 @@
+package scacchi;
+
+public class Position {
+	int i;
+	int j;
+
+	public Position(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+}
