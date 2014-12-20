324d83abb269000f43a0ac81be6d7ed7a803855f
Deste <dester94@gmail.com>
2014-12-20 10:42:46 +0100

    commit2

--- Scacchi/src/scacchi/Pedina.java
+++ Scacchi/src/scacchi/Pedina.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public abstract class Pedina {
 	String name;
 	int color; //1-2
-	
+	int c;
 	public abstract ArrayList<Position> possibleMoves(Position position);
 	
 }
