f627bd949a7b56abcef59e2b65ed348f34681bf4
Davide <lm10263@gmail.com>
2015-01-09 18:01:23 +0100

    scambio pedine re-regina. la classe non è finita

--- Scacchi/src/scacchi/Scacchiera.java
+++ Scacchi/src/scacchi/Scacchiera.java
@@ -12,8 +12,8 @@ public class Scacchiera {
 		scacchiera[0][0] = new Torre(Colore.NERO);
 		scacchiera[0][1] = new Cavallo(Colore.NERO);
 		scacchiera[0][2] = new Alfiere(Colore.NERO);
-		scacchiera[0][3] = new Re(Colore.NERO);
-		scacchiera[0][4] = new Regina(Colore.NERO);
+		scacchiera[0][3] = new Regina(Colore.NERO);
+		scacchiera[0][4] = new Re(Colore.NERO);
 		scacchiera[0][5] = new Alfiere(Colore.NERO);
 		scacchiera[0][6] = new Cavallo(Colore.NERO);
 		scacchiera[0][7] = new Torre(Colore.NERO);
@@ -24,8 +24,8 @@ public class Scacchiera {
 		scacchiera[7][0] = new Torre(Colore.BIANCO);
 		scacchiera[7][1] = new Cavallo(Colore.BIANCO);
 		scacchiera[7][2] = new Alfiere(Colore.BIANCO);
-		scacchiera[7][3] = new Re(Colore.BIANCO);
-		scacchiera[7][4] = new Regina(Colore.BIANCO);
+		scacchiera[7][3] = new Regina(Colore.BIANCO);
+		scacchiera[7][4] = new Re(Colore.BIANCO);
 		scacchiera[7][5] = new Alfiere(Colore.BIANCO);
 		scacchiera[7][6] = new Cavallo(Colore.BIANCO);
 		scacchiera[7][7] = new Torre(Colore.BIANCO);
@@ -37,10 +37,24 @@ public class Scacchiera {
 	 * 
 	 * @param i la riga della scacchiera
 	 * @param j la colonna della scacchiera
-	 * @return la pedina in 
+	 * @return la pedina in posizione (i,j)
 	 */
 	public Pedina getPedina(int i, int j){
 		return scacchiera[i][j];
+	}
+	
+	/**
+	 * 
+	 * @param p la posizione della pedina
+	 * @return un array di interi dove sono indicate le 
+	 */
+	public int[][] getMoves(Position p){
+		int[][] moves = new int[8][8];
+		
+		
+		
+		
+		return moves;
 	}
 	
 	
