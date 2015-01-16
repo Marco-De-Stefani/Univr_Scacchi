5d56f8edb4ad8f07d9445ef52722fa3cc6a6d3d1
Tommaso <tommaso.dalfior94@gmail.com>
2015-01-16 17:37:03 +0100

    agg classe re

--- Scacchi/src/scacchi/Re.java
+++ Scacchi/src/scacchi/Re.java
@@ -41,5 +41,42 @@ public class Re extends Pedina{
 		return positions;
 		
 	}
+	
+	public int[][] mossePossibili(Position posPedina, Pedina[][] scacchiera) {
+		//Position position = new Position(riga, colonna);
+		ArrayList<Position> positions = new ArrayList<Position>();
+		int[][] mosse = new int[8][8];
+		
+		positions.add(new Position(posPedina.getRiga() + 1, posPedina.getColonna())); 		// avanti o indietro
+		positions.add(new Position(posPedina.getRiga() - 1, posPedina.getColonna())); 		// avanti o indietro
+		
+		positions.add(new Position(posPedina.getRiga(), posPedina.getColonna() + 1)); 		//dx o sx
+		positions.add(new Position(posPedina.getRiga(), posPedina.getColonna() - 1));		//dx o sx
+		
+		positions.add(new Position(posPedina.getRiga() + 1, posPedina.getColonna() + 1));	// diagonale dx o alto o basso
+		positions.add(new Position(posPedina.getRiga() - 1, posPedina.getColonna() + 1));	// diagonale dx o alto o basso
+		
+		positions.add(new Position(posPedina.getRiga() + 1, posPedina.getColonna() - 1));	// diagonale sx o alto o basso
+		positions.add(new Position(posPedina.getRiga() - 1, posPedina.getColonna() - 1));	// diagonale sx o alto o basso
+		
+
+			for (int i = 0; i < positions.size(); i++) {
+				Position pos=positions.get(i);
+				if (pos.getRiga() <= 0 || pos.getColonna() <= 0 || pos.getRiga() > nRighe || pos.getColonna() > nColonne) {
+					positions.remove(pos);
+				}
+			}
+			
+			for(Position p : positions)
+				if(scacchiera[p.getRiga()][p.getColonna()] == null){
+					mosse[p.getRiga()][p.getColonna()] = 1;
+				}else{
+					if(scacchiera[p.getRiga()][p.getColonna()].getColore() != this.getColore())
+						mosse[p.getRiga()][p.getColonna()] = 2;
+				}
+
+		return mosse;
+		
+	}
 
 }
