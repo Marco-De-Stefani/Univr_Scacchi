a924ca4186b28c4d1564ea36c203a83edac4f29e
Davide <lm10263@gmail.com>
2015-01-16 16:12:44 +0100

    update alfiere

--- Scacchi/src/scacchi/Alfiere.java
+++ Scacchi/src/scacchi/Alfiere.java
@@ -52,4 +52,100 @@ public class Alfiere extends Pedina {
 		
 		return punti;
 	}
+	
+	
+	
+	public int[][] mossePossibili(Position posPedina, Pedina[][] scacchiera){
+		int[][] mosse = new int[8][8];
+		int k;
+		int l;
+		
+		//l'alfiere può muoversi in obliquo
+		//obliquo dx su
+		k = posPedina.getRiga() - 1;
+		l = posPedina.getColonna() + 1;
+		//while(k > 0 || l < nColonne){
+		//	punti.add(new Position(k, l));
+		//	k--;
+		//	l++;
+		//}
+		while(k >= 0 || l < nColonne){
+			if(scacchiera[k][l] == null){
+				mosse[k][l] = 1;
+				k--;
+				l++;
+			}else{
+				if(scacchiera[k][l].getColore() != this.getColore())
+					mosse[k][l] = 2;
+				break;
+			}
+		}
+		
+		
+		//obliquo sx su
+		k = posPedina.getRiga() - 1;
+		l = posPedina.getColonna() - 1;
+		//while(k > 0 || l > 0){
+		//	punti.add(new Position(k, l));
+		//	k--;
+		//	l--;
+		//}
+		while(k >= 0 || l >= 0){
+			if(scacchiera[k][l] == null){
+				mosse[k][l] = 1;
+				k--;
+				l--;
+			}else{
+				if(scacchiera[k][l].getColore() != this.getColore())
+					mosse[k][l] = 2;
+				break;
+			}
+		}
+		
+		
+		//obliquo dx giu
+		k = posPedina.getRiga() + 1;
+		l = posPedina.getColonna() + 1;
+		//while(k < nRighe || l < nColonne){
+		//	punti.add(new Position(k, l));
+		//	k++;
+		//	l++;
+		//}
+		while(k < nRighe || l < nColonne){
+			if(scacchiera[k][l] == null){
+				mosse[k][l] = 1;
+				k++;
+				l++;
+			}else{
+				if(scacchiera[k][l].getColore() != this.getColore())
+					mosse[k][l] = 2;
+				break;
+			}
+		}
+		
+		//obliquo sx giu
+		k = posPedina.getRiga() + 1;
+		l = posPedina.getColonna() - 1;
+		//while(k < nRighe || l > 0){
+		//	punti.add(new Position(k, l));
+		//	k++;
+		//	l--;
+		//}
+		while(k < nRighe || l >= 0){
+			if(scacchiera[k][l] == null){
+				mosse[k][l] = 1;
+				k++;
+				l--;
+			}else{
+				if(scacchiera[k][l].getColore() != this.getColore())
+					mosse[k][l] = 2;
+				break;
+			}
+		}
+		
+		
+		return mosse;
+	}
+	
+	
 }
