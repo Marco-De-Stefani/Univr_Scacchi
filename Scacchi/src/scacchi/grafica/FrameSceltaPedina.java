faf98cf18a4e16de492522c8b06cc6a049d17379
Tommaso <tommaso.dalfior94@gmail.com>
2015-02-18 10:53:11 +0100

    miglio

--- Scacchi/src/scacchi/grafica/FrameSceltaPedina.java
+++ Scacchi/src/scacchi/grafica/FrameSceltaPedina.java
@@ -10,11 +10,11 @@ public class FrameSceltaPedina extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public FrameSceltaPedina() {
-		super("Promozione");
+		super("Promozione pedone");
 		PanelSceltaPedina psppp = new PanelSceltaPedina();
 		psppp.setColore(Colore.NERO);
 		add(psppp);
-		this.setSize(250, 250);
+		this.setSize(250, 220);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(false);
 	}
