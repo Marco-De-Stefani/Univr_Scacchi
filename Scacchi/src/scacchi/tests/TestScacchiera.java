2dd7443196d35664f307b726a55d2c1ddb38f8ce
Davide <lm10263@gmail.com>
2015-04-10 11:50:49 +0200

    upd test

--- Scacchi/src/scacchi/tests/TestScacchiera.java
+++ Scacchi/src/scacchi/tests/TestScacchiera.java
@@ -26,7 +26,7 @@ public class TestScacchiera {
 	
 	@Test
 	public void testGetMoves() {
-		Scacchiera scacchiera = new Scacchiera(false);
+		Scacchiera scacchiera = new Scacchiera(true);
 		int [][] mosse = new int[8][8];
 		
 //		for(int i = 0; i<8; i++){
@@ -34,12 +34,13 @@ public class TestScacchiera {
 //				System.out.print(mosse[i][j] + "-");
 //			System.out.println();
 //		}
-		mosse = scacchiera.getMoves(new Position(1,1));
-		Assert.assertFalse(mosse[2][1] == 1);	//muovono prima le pedine bianche
+		mosse = scacchiera.getMoves(new Position(0,3));
+		Assert.assertFalse(mosse[1][3] == 1);	//muovono prima le pedine bianche
 		
-		mosse = scacchiera.getMoves(new Position(6,1));
-		Assert.assertTrue(mosse[5][1] == 1);
-		
+		mosse = scacchiera.getMoves(new Position(5,6));
+		Assert.assertFalse(mosse[1][3] == 1);
+		Assert.assertTrue(mosse[1][2] == 1);
+		Assert.assertTrue(mosse[4][7] == 2);
 	}
 	
 	
@@ -52,5 +53,39 @@ public class TestScacchiera {
 		Assert.assertFalse(scacchiera.move(new Position(1,2), new Position(5,2), null));
 		Assert.assertTrue(scacchiera.move(new Position(1,2), new Position(2,2), null));
 	}
-
+	
+	
+	@Test
+	public void testCanMove() {
+		Scacchiera scacchiera = new Scacchiera(true);
+		
+		scacchiera.move(new Position(5,6), new Position(4,7), null);
+		Assert.assertFalse(scacchiera.canMove(new Position(2,1), new Position(3,1)));
+		Assert.assertTrue(scacchiera.canMove(new Position(0,3), new Position(0,4)));
+	}
+	
+	
+	@Test
+	public void testScacco() {
+		Scacchiera scacchiera = new Scacchiera(true);
+		
+		scacchiera.move(new Position(5,6), new Position(4,7), null);
+		Assert.assertTrue(scacchiera.scacco() == -1);	//scacco a re nero
+		
+		scacchiera.move(new Position(0,3), new Position(0,4), null);
+		Assert.assertTrue(scacchiera.scacco() == 0);	//nessuno scacco
+		
+		scacchiera.move(new Position(4,7), new Position(3,6), null);
+		scacchiera.move(new Position(4,3), new Position(4,4), null);
+		Assert.assertTrue(scacchiera.scacco() == 1);	//scacco a re bianco
+	}
+	
+	
+	@Test
+	public void testScaccoMatto() {
+		
+	}
+	
+	
+	
 }
